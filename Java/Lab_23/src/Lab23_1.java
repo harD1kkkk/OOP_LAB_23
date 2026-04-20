f087b53fd55f66ebd80c1ccf727e2d8c3e3b92d3
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2026-04-20 11:44:18 +0300

    Refactor Lab23_1 to include geometric figures

--- Java/Lab_23/src/Lab23_1.java
+++ Java/Lab_23/src/Lab23_1.java
@@ -1,5 +1,63 @@
 public class Lab23_1 {
 
+    abstract static class Figure {
+        abstract double getArea();
+        abstract double getPerimeter();
+    }
+
+    static class Rectangle extends Figure {
+        double width, height;
+
+        Rectangle(double w, double h) {
+            width = w;
+            height = h;
+        }
+
+        double getArea() {
+            return width * height;
+        }
+
+        double getPerimeter() {
+            return 2 * (width + height);
+        }
+    }
+
+    static class Circle extends Figure {
+        double radius;
+
+        Circle(double r) {
+            radius = r;
+        }
+
+        double getArea() {
+            return Math.PI * radius * radius;
+        }
+
+        double getPerimeter() {
+            return 2 * Math.PI * radius;
+        }
+    }
+
+    static class Trapezium extends Figure {
+        double a, b, c, d, height;
+
+        Trapezium(double a, double b, double c, double d, double h) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.height = h;
+        }
+
+        double getArea() {
+            return (a + b) / 2 * height;
+        }
+
+        double getPerimeter() {
+            return a + b + c + d;
+        }
+    }
+
     interface IInterface<T> {
         void setValue(T value);
         T getValue();
@@ -18,8 +76,19 @@ public class Lab23_1 {
     }
 
     public static void run() {
-        MyClass<Integer> obj = new MyClass<>();
-        obj.setValue(100);
-        System.out.println(obj.getValue());
+        Figure f1 = new Rectangle(5, 4);
+        Figure f2 = new Circle(3);
+        Figure f3 = new Trapezium(3, 5, 4, 4, 2);
+
+        MyClass<Figure> obj = new MyClass<>();
+
+        obj.setValue(f1);
+        System.out.println(obj.getValue().getArea());
+
+        obj.setValue(f2);
+        System.out.println(obj.getValue().getPerimeter());
+
+        obj.setValue(f3);
+        System.out.println(obj.getValue().getArea());
     }
 }
